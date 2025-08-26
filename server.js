3b40a84b06b2f5180135b4883b37e0112108a4ec
JuliPapp <84880514+JuliPapp@users.noreply.github.com>
2025-08-26 09:18:29 -0300

    Actualizar server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Configuración de menú
 const MENU_ITEMS = [
-  'filet de merluza con puré'
+  'filet de merluza con puré',
   'Tarta JyQ',
   'Tarta capresse',
   'Tarta brócoli',
